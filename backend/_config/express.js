c6e3460406c2845714c57c93eb0a80c5cf9a10a2
Emanoel Trevisol <emanoeltrevisol@MacBook-Pro.local>
2019-08-12 20:42:29 -0300

    Added cors config

--- backend/_config/express.js
+++ backend/_config/express.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const cors = require("cors");
 
 const app = express();
 
@@ -13,5 +14,24 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+const env = process.env.NODE_ENV || "development";
+
+let whitelist = [];
+if (env === "development") {
+  whitelist = ["http://localhost:3000"];
+} else if (env === "homolog") {
+  whitelist = [];
+} else if (env === "production") {
+  whitelist = [];
+}
+
+console.log("CORS whitelist:", whitelist);
+
+app.use(
+  cors({
+    origin: whitelist
+  })
+);
 
 module.exports = app;
