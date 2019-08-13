714e47443e2d69ef5e3448ffdb34028de75d62bf
Emanoel Trevisol <emanoeltrevisol@MacBook-Pro.local>
2019-08-12 21:05:11 -0300

    Added  basic configs for files and routes

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,30 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { store as movies } from "./modules/movies";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
+const store = new Vuex.Store({
+  modules: {
+    movies
+  },
   state: {},
   mutations: {},
-  actions: {}
+  actions: {},
+  getters: {},
+  strict: process.env.NODE_ENV !== "production"
 });
+
+// eslint-disable-next-line
+if (module.hot) {
+  // eslint-disable-next-line
+  module.hot.accept(["@/modules/movies/store"], () => {
+    store.hotUpdate({
+      modules: {
+        movies: require("@/modules/movies/store").default
+      }
+    });
+  });
+}
+
+export default store;
